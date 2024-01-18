e28cf28449baf390f74430aba37db17a766d7ad4
Javed Ali <ryx739@gmail.com>
2024-01-18 19:08:38 +0500

    Visual Studio 2022 Version 17.8.4 .NET8 MAUI Template

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace InstallmentsPointOfSale
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
